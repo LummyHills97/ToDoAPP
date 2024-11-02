3857a69165f625603d992f9bb37eea170a3d0da9
LummyHills97 <olumidayolumiday@gmail.com>
2024-11-02 06:12:46 +0100

    To Do App

---
+++ lib/Data/database.dart
@@ -0,0 +1,30 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class TodoDatabase {
+  List toDoList = [];
+
+  //reference the box
+  final _myBox = Hive.box('mybox');
+
+  // run this method if this is the first time opening this app
+  void createInitialData() {
+   toDoList = [
+      ["Make Tutorial", false],
+      ["Do Exercise", false],
+    ];
+  }
+
+  // load the data from the database
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+
+  }
+
+  // update the database
+  void updateDatabase() {
+    _myBox.put("TODOLIST", toDoList);
+
+  }
+
+
+}
